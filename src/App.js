81d204d1e341a2b25bc5fd483ea9458c86ba9ed0
JaiJoshi123 <jaijoshi0310@gmail.com>
2023-04-06 19:23:56 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import axios from "axios";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
+  const [query, setQuery] = useState();
+  const [results, setResults] = useState([]);
+  const inputRef = useRef();
+  const tenor_api_base = "https://g.tenor.com/v1";
+  const tenor_api_seacrh = tenor_api_base + "/search";
+
+  const handleSearch = async () => {
+    setQuery(inputRef.current.value);
+    const searchResults = await axios.get(tenor_api_seacrh, {
+      params: {
+        q: inputRef.current.value,
+        key: "LIVDSRZULELA",
+        limit: 20,
+      }
+    });
+    console.log(searchResults.data.results)
+    setResults(prev => searchResults.data.results);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="container m-3">
+          <div class="col-8 d-flex flex-row m-3">
+            <input type="text" ref={inputRef} class="form-control" placeholder="Search GIFs" />
+            <button class="btn btn-primary" onClick={() => handleSearch()}>search</button>
+          </div>
+          <div class="row">
+            {
+              results == null ?
+                <></>
+                :
+                results.map((g, i) => (
+                  <div class="col-4">
+                    <img src={g.media[0].gif.url} alt="logo" />
+                  </div>
+                ))
+            }
+
+          </div>
+        </div>
       </header>
     </div>
   );
